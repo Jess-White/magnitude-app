d13b9e6cca208245b3941f2d70a4dd4abf157f3b
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2020-07-22 09:31:29 -0500

    updated State

--- src/components/State.js
+++ src/components/State.js
@@ -1,11 +1,26 @@
 import React from "react";
 
+function handleToggleState() {
+  const toggleState = toggleState;
+  toggleState === !toggleState
+
+}
+
+
+
 function State({state}) {
   return (
-    <div>
-      <h4>State: {state.state}</h4>
-      <h4>Number of deaths:  <span style={{color: "red"}}>{state.death}</span></h4>
-    </div>
+        <div onClick={handleToggleState()}>State: {state.state}>
+          <div>
+          {this.toggleState === true && 
+            <h4>Number of deaths:  <span style={{color: "red"}}>{state.death}</span></h4>
+            <h4>Number of positive tests:  <span style={{color: "red"}}>{state.positive}</span></h4>
+            <h4>Number of negative tests:  <span style={{color: "red"}}>{state.negative}</span></h4>
+            <h4>Number of tests total:  <span style={{color: "red"}}>{state.total}</span></h4>
+            <h4>Last updated:  <span style={{color: "red"}}>{state.date}</span></h4>
+            }
+          </div>
+        </div>
   )
     
 }
