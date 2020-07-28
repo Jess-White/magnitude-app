773f64233c4701b4d21b3318075b26508b81cdaf
mike <mikemcfaddin@gmail.com>
2020-07-27 20:54:36 -0500

    adds chart.js bar chart to state modal

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React from 'react';
 import Header from "./components/Header";
 import Dashboard from "./components/Dashboard";
+// import Tracker from "./components/Tracker"
 import Footer from "./components/Footer";
 // import GridMap from "./components/GridMap";
 
@@ -12,6 +13,7 @@ function App() {
       <Header />
       {/*<GridMap />*/}
       <Dashboard />
+      {/* <Tracker /> */}
       <Footer />
     </div>
   );
