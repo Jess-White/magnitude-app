b502ce2695d11c4d8611ca0de5e0ea595416243b
EatYourSoup7 <r.kondratowicz1@gmail.com>
2020-08-17 15:03:58 -0500

    kondratowicz

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -12,7 +12,7 @@ function Dashboard() {
   const [query, setQuery] = useState('')
 
   function fetchData() {
-    fetch("https://covidtracking.com/api/states")
+    fetch("https://api.covidtracking.com/v1/states/current.json")
       .then(response => response.json())
       .then(response => {
         const data = response.map(state => {
